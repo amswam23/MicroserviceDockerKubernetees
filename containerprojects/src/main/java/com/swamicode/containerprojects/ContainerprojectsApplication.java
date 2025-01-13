c04c573b2c908e6905f0ddd34b12d10dbdbfd981
amswam23 <amswam23@gmail.com>
2025-01-13 16:33:48 -0500

    Initial project setup

---
+++ containerprojects/src/main/java/com/swamicode/containerprojects/ContainerprojectsApplication.java
@@ -0,0 +1,13 @@
+package com.swamicode.containerprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerprojectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerprojectsApplication.class, args);
+	}
+
+}
